7758a00890c27bfdac9ed1d55315f17be04c7eb7
pazuzu71 <genchez.tula@gmail.com>
2023-02-14 20:54:43 +0300

    Сделал .env для хранения переменных окружения

--- create_bot.py
+++ create_bot.py
@@ -1,6 +1,15 @@
 from aiogram import Bot, Dispatcher
-from config import TOKEN
+from environs import Env
 
 
+env = Env()  # Создаем экземпляр класса Env
+env.read_env()  # Методом read_env() читаем файл .env и загружаем из него переменные в окружение
+
+TOKEN = env('TOKEN')  # Сохраняем значение переменной окружения в переменную TOKEN
+ID = env.int('ID')  # Преобразуем значение переменной окружения к типу int
+# и сохраняем в переменной ID
+
+# print(TOKEN)
+# print(ID)
 bot: Bot = Bot(token=TOKEN)
 dp: Dispatcher = Dispatcher()
